ef49bfd5302ada89fc3275af42601cb7a6ff3b7d
teperyaemo <rokudenas@bk.ru>
2021-10-30 23:54:49 +0300

    Добавьте файлы проекта.

---
+++ travelRuski/hotelPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace travelRuski
+{
+    /// <summary>
+    /// Логика взаимодействия для hotelPage.xaml
+    /// </summary>
+    public partial class hotelPage : Page
+    {
+        public hotelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void editBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.mainFrame.Navigate(new addEditPage());
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.mainFrame.Navigate(new addEditPage());
+        }
+
+        private void delBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Сделаем вид что строка удалилась");
+        }
+    }
+}
---
+++ travelRuski/toursPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace travelRuski
+{
+    /// <summary>
+    /// Логика взаимодействия для toursPage.xaml
+    /// </summary>
+    public partial class toursPage : Page
+    {
+        public toursPage()
+        {
+            InitializeComponent();
+        }
+
+        private void toursBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.mainFrame.Navigate(new trueTourPage());
+        }
+
+        private void hotelsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.mainFrame.Navigate(new hotelPage());
+        }
+    }
+}
